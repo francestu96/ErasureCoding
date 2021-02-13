56948164bbed4415e173423933fde505e5a65fe3
Francesco Stucci <francesco.stucci@horsa.it>
2021-02-13 14:24:21 +0100

    ok

--- backup.py
+++ backup.py
@@ -20,7 +20,7 @@ YEAR = 365 * DAY
 ### SYSTEM PARAMETERS
 
 N = 10 # number of servers storing data
-K = 7 # number of blocks needed to recover data
+K = 9 # number of blocks needed to recover data
 
 # parameters about the node backing up the data
 NODE_LIFETIME = 100 * DAY  # average time before node crashes and loses data
@@ -42,38 +42,38 @@ BLOCK_SIZE = DATA_SIZE / K
 UPLOAD_DURATION = BLOCK_SIZE / UPLOAD_SPEED
 DOWNLOAD_DURATION = BLOCK_SIZE / DOWNLOAD_SPEED
 
-for k in range(K, N + 1):
-    plot_speed = []
-    plot_time = []
-    for upload_speed, download_speed in zip(range(5*KB, UPLOAD_SPEED + 5*KB, 5*KB), range(20*KB, DOWNLOAD_SPEED + 20*KB, 20*KB)):
-        UPLOAD_DURATION = BLOCK_SIZE / upload_speed
-        DOWNLOAD_DURATION = BLOCK_SIZE / download_speed
+# for k in range(K, N):
+plot_speed = []
+plot_time = []
+for upload_speed, download_speed in zip(range(10*KB, UPLOAD_SPEED + 10*KB, 10*KB), range(40*KB, DOWNLOAD_SPEED + 40*KB, 40*KB)):
+    UPLOAD_DURATION = BLOCK_SIZE / upload_speed
+    DOWNLOAD_DURATION = BLOCK_SIZE / download_speed
 
-        state = State(N, k, NODE_UPTIME, NODE_DOWNTIME, NODE_LIFETIME, SERVER_UPTIME, SERVER_DOWNTIME, SERVER_LIFETIME, UPLOAD_DURATION, DOWNLOAD_DURATION)
-        events = state.events
+    state = State(N, K, NODE_UPTIME, NODE_DOWNTIME, NODE_LIFETIME, SERVER_UPTIME, SERVER_DOWNTIME, SERVER_LIFETIME, UPLOAD_DURATION, DOWNLOAD_DURATION)
+    events = state.events
 
-        try:
-            while events:
-                t, event = heappop(events)
-                if t > MAXT:
-                    break
+    try:
+        while events:
+            t, event = heappop(events)
+            if t > MAXT:
+                break
 
-                state.t = t
-                event.process(state)
-        except GameOver:
-            log(state, upload_speed/KB, download_speed/MB, k, YEAR, "Game over")
-            print(f"Game over after {t/YEAR:.2f} years!")
-        else:
-            print(f"Data safe for {t/YEAR:.2f} years!")
-        finally:
-            plot_time.append(t/YEAR)
-            plot_speed.append((upload_speed + upload_speed) / 2 / KB)
+            state.t = t
+            event.process(state)
+    except GameOver:
+        log(state, upload_speed/KB, download_speed/MB, K, YEAR, "Game over")
+        print(f"Game over after {t/YEAR:.2f} years!")
+    else:
+        print(f"Data safe for {t/YEAR:.2f} years!")
+    finally:
+        plot_time.append(t/YEAR)
+        plot_speed.append((upload_speed + upload_speed) / 2 / KB)
 
-    plt.plot(plot_time, plot_speed, label=f"Redundancy: {N/k:.2f}")
+plt.barh(plot_speed, plot_time, height=10, label=f"Redundancy: {N/K:.2f}")
 
 plt.xlabel("Years")
 plt.ylabel("Avg KB speed")
 plt.title(f'Simulation of {MAXT/YEAR:.0f} years')
-plt.grid()
+plt.xlim(0, MAXT/YEAR)
 plt.legend()
 plt.show()
